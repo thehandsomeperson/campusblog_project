601cc4de4d94ce70c3cc3e00fd87819b257ee40a
Zelin Zhu <3088451Z@student.gla.ac.uk>
2026-03-11 23:35:32 +0000

    finish the base.html and register view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+#we use the NewUserCreationForm created in form.py which is inherited from UserCreationForm
+from .forms import NewUserCreationForm
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        # user submit the form and put data into NewUserCreationForm
+        form = NewUserCreationForm(request.POST)
+        if form.is_valid():
+            #after proof passed, store new user and redirect to login page
+            form.save()
+            return redirect('accounts:login')
+        # if validation failure, continue to render the register page
+    else:
+        # When user open the page, give them an empty page
+        form = NewUserCreationForm()
+
+    # GET or POST failure，then render the register page again
+    return render(request, 'accounts/register.html', {'form': form})
+
